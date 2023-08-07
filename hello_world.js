3c29c072de1d3cf17b21fc31b53de7e53717756e
Pravind <parvindraj004@gmail.com>
2023-08-07 18:41:42 +0530

    token code

--- hello_world.js
+++ hello_world.js
@@ -1,5 +1,6 @@
-
+//localhost:3000/api/users    <----- Run this code and Hit this URL in Postman
 var express=require('express');
+const users = require('./Users');
 var app = express();
 
 app.use(express.json())
@@ -7,9 +8,11 @@ app.use(express.urlencoded({extended:false}))
 
 app.use('/api/users',require('./routes/api/users'))
 
+console.log(users);
 app.listen(3000, ()=>{
     console.log('Server Started on port 3000')
 })
+
 // app.get('/',function(req,res){
 //     res.send('Hello King');
 // })
