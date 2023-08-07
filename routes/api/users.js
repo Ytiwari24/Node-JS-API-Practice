bd570a755b862d920ab9f3f25979e6110dc6da77
Pravind <parvindraj004@gmail.com>
2023-08-07 17:51:19 +0530

    get post update delete all cases

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,25 +1,67 @@
-const express=require('express')
-const router=express.Router()
-const uuid=require('uuid')
-let users=require('../../Users')
+const express = require('express')
+const router = express.Router()
+const uuid = require('uuid')
+let users = require('../../Users')
 
 ///********GET ALL USERS**********///
-router.get('/',(req,res) => {
+router.get('/', (req, res) => {
     res.json(users);
 });
-
 ///********GET ALL USERS BY ID**********///
-router.get('/:id',(req,res) =>{
-    const found = users.some(user =>user.id ===parseInt(req.params.id))
+router.get('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
 
-    if(found){
-        res.json(users.filter(user => user.id ===parseInt(req.params.id)))
-    }else{
+    if (found) {
+        res.json(users.filter(user => user.id === parseInt(req.params.id)))
+    } else {
         res.sendStatus(400)
+    }
+});
+
+///********CREATE NEW USER**********///
+router.post('/', (req, res) => {
+    const newUser = {
+        id: uuid.v4(),
+        name: req.body.name,
+        email: req.body.email
+    }
+    if (!newUser.name || !newUser.email) {
+        return res.sendStatus(400)
+    }
+    users.push(newUser)
+    res.json(users)
+})
+
+///********UPDATE USER**********///
+router.put('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+
+    if (found) {
+        const updateUser = req.body;
+        users.forEach(user => {
+            if (user.id === parseInt(req.params.id)) {
+                user.name = updateUser.name ? updateUser.name : user.name
+                user.email = updateUser.email ? updateUser.email : user.email
+                res.json({ msg: 'User Updated', user })
+            }
+        })
     }
 })
 
+///********DELETE USER**********///
+router.delete('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+
+    if (found) {
+        users = users.filter((user) => user.id !== parseInt(req.params.id));
+        res.json({
+            msg: 'User Deleted Successfully',
+            users,
+        });
+    } else {
+        res.sendStatus(400);
+    }
+});
 
 
-
-module.exports =router
+module.exports = router
